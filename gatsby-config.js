3a33f94cc29462d51fac5f570473f65752264df8
Casper Kuijjer <casper@kuijjer.com>
2020-05-22 08:15:14 +0200

    exclude /typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
-        exclude: [],
+        exclude: ['/typography'],
         query: `
           {
             site {
